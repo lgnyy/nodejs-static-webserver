1179d4a9db5759a8f8e52f830c31b5995ccfe6f8
lijinchun <jcli@tendyron.com>
2023-04-30 11:21:26 +0800

    把命令配置移到app.js里 在start增加输入参数（ssl配置），支持https

--- app.js
+++ app.js
@@ -1,3 +1,16 @@
 const StaticServer = require('./static-server');
 
-(new StaticServer()).start();
+var options = require( "yargs" )
+    .option( "p", { alias: "port",  describe: "Port number", type: "number" } )
+    .option( "r", { alias: "root", describe: "Static resource directory", type: "string" } )
+    .option( "i", { alias: "index", describe: "Default page", type: "string" } )
+    .option( "c", { alias: "cachecontrol", default: true, describe: "Use Cache-Control", type: "boolean" } )
+    .option( "e", { alias: "expires", default: true, describe: "Use Expires", type: "boolean" } )
+    .option( "t", { alias: "etag", default: true, describe: "Use ETag", type: "boolean" } )
+    .option( "l", { alias: "lastmodified", default: true, describe: "Use Last-Modified", type: "boolean" } )
+    .option( "m", { alias: "maxage", describe: "Time a file should be cached for", type: "number" } )
+    .help()
+    .alias( "?", "help" )
+    .argv;
+
+(new StaticServer(options)).start();
--- static-server.js
+++ static-server.js
@@ -1,4 +1,4 @@
-const http = require('http');
+//const http = require('http');
 const path = require('path');
 const fs = require('fs');
 const url = require('url');
@@ -6,23 +6,10 @@ const zlib = require('zlib');
 const mime = require('./mime');
 const config = require('./config/default');
 
-var options = require( "yargs" )
-    .option( "p", { alias: "port",  describe: "Port number", type: "number" } )
-    .option( "r", { alias: "root", describe: "Static resource directory", type: "string" } )
-    .option( "i", { alias: "index", describe: "Default page", type: "string" } )
-    .option( "c", { alias: "cachecontrol", default: true, describe: "Use Cache-Control", type: "boolean" } )
-    .option( "e", { alias: "expires", default: true, describe: "Use Expires", type: "boolean" } )
-    .option( "t", { alias: "etag", default: true, describe: "Use ETag", type: "boolean" } )
-    .option( "l", { alias: "lastmodified", default: true, describe: "Use Last-Modified", type: "boolean" } )
-    .option( "m", { alias: "maxage", describe: "Time a file should be cached for", type: "number" } )
-    .help()
-    .alias( "?", "help" )
-    .argv;
-
 const hasTrailingSlash = url => url[url.length - 1] === '/';
 
 class StaticServer {
-    constructor() {
+    constructor(options) {
         this.port = options.p || config.port;
         this.root = options.r || config.root;
         this.indexPage = options.i || config.indexPage;
@@ -211,11 +198,21 @@ class StaticServer {
         });
     }
 
-    start() {
-        http.createServer((req, res) => {
+    start(options) {
+		let server;
+        const callback = ((req, res) => {
             const pathName = path.join(this.root, path.normalize(req.url));
             this.routeHandler(pathName, req, res);
-        }).listen(this.port, err => {
+        });
+		if (options && (options.pfx || options.key)){
+			const http = require("https");
+			server = http.createServer(options, callback);
+		}else{
+			const http = require("http");
+			server = http.createServer(callback);			
+		}
+		
+		server.listen(this.port, err => {
             if (err) {
                 console.error(err);
                 console.info('Failed to start server');
@@ -223,6 +220,7 @@ class StaticServer {
                 console.info(`Server started on port ${this.port}`);
             }
         });
+		return server;
     }
 }
 
